a1859fbd311fc2d801bd727538c4724452a2d328
Souleyman BEN HADJ BELGACEM <souleyman@MacBook-Pro-de-Souleyman.local>
2023-09-20 10:56:45 +0200

    create react app

---
+++ my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./chokbar.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Chokbar</p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=X6UcMncuuwU&ab_channel=Kartofelsalat"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          🕺🏽🕺🏽🕺🏽🕺🏽
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
